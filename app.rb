ba51410523723fc8bd8504f9c925625fbba0fe46
zamami <s1413115@gmail.com>
2023-06-18 13:45:55 +0900

    jsonファイルの保存先を変更。idをuuidを使って生成。

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'securerandom'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
@@ -12,14 +13,14 @@ helpers do
   end
 
   def set_all_files
-    @files = Dir.glob('public/notes/*').map do |file|
+    @files = Dir.glob('../json/*').map do |file|
       JSON.parse(File.read(file))
     end
   end
 
   def make_json_file
-    id = Time.now.strftime('%Y%m%d_%H_%M_%S') # メモごとにファイルを保存
-    File.open("public/notes/#{id}_note.json", 'w') do |file| # 新規投稿フォームの内容をjsonに保存
+    id = "#{Time.now.strftime('%Y%m%d_%H_%M_%S')}_#{SecureRandom.uuid}"
+    File.open("../json/#{id}_note.json", 'w') do |file| # 新規投稿フォームの内容をjsonに保存
       hash = { id:, title: params[:title], content: params[:contents] }
       JSON.dump(hash, file)
     end
@@ -47,21 +48,21 @@ get '/memos/:id' do # 詳細画面
 end
 
 delete '/memos/:id' do # 削除機能
-  File.delete("public/notes/#{params[:id]}_note.json")
+  File.delete("../json/#{params[:id]}_note.json")
   redirect to('/memos')
 end
 
 get '/memos/:id/edit' do # 編集画面
-  edit_memo = File.open("public/notes/#{params[:id]}_note.json", 'r+').read
+  edit_memo = File.open("../json/#{params[:id]}_note.json", 'r+').read
   @edit_memo = JSON.parse(edit_memo)
   erb :edit_memo
 end
 
 patch '/memos/:id' do # 編集機能
-  file = File.read("public/notes/#{params[:id]}_note.json")
+  file = File.read("../json/#{params[:id]}_note.json")
   update_file = JSON.parse(file)
   update_file['title'] = params['title']
   update_file['content'] = params['content']
-  File.open("public/notes/#{update_file['id']}_note.json", 'w') { |f| JSON.dump(update_file, f) }
+  File.open("../json/#{update_file['id']}_note.json", 'w') { |f| JSON.dump(update_file, f) }
   redirect to('/memos')
 end
