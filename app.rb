8b10a8d74070eab470a4d38d6b4b27dda9247b7e
zamami <s1413115@gmail.com>
2023-07-13 23:00:56 +0900

    空白の削除、空行の追加、SQL文のコロン追加

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ get '/memos' do # トップ画面
 end
 
 post '/memos' do # 新規メモデータの受け取り
-  sql = 'INSERT INTO memos(title, content, created_time) VALUES ($1, $2, $3)'
+  sql = 'INSERT INTO memos(title, content, created_time) VALUES ($1, $2, $3);'
   connection.exec_params(sql, [params[:title], params[:content], Time.new])
   redirect to('/memos')
 end
@@ -29,25 +29,25 @@ get '/memos/new' do # 新規登録
 end
 
 get '/memos/:id' do # 詳細画面
-  sql = 'SELECT * FROM memos WHERE id = $1'
+  sql = 'SELECT * FROM memos WHERE id = $1;'
   @memo = connection.exec_params(sql, [params[:id]]).first
   erb :show
 end
 
 delete '/memos/:id' do # 削除機能
-  sql = 'DELETE FROM memos WHERE id = $1'
+  sql = 'DELETE FROM memos WHERE id = $1;'
   connection.exec_params(sql, [params[:id]])
   redirect to('/memos')
 end
 
 get '/memos/:id/edit' do # 編集画面
-  sql = 'SELECT * FROM memos WHERE id = $1'
+  sql = 'SELECT * FROM memos WHERE id = $1;'
   @memo = connection.exec_params(sql, [params[:id]]).first
   erb :edit
 end
 
 patch '/memos/:id' do # 編集機能
-  sql = 'UPDATE memos SET title = $1, content = $2 WHERE id = $3'
+  sql = 'UPDATE memos SET title = $1, content = $2 WHERE id = $3;'
   connection.exec_params(sql, [params[:title], params[:content], params[:id]])
   redirect to('/memos')
 end
